f0894eea5453f5583a4bbe9837e09eb89b9d9371
Bozhidar Rusev <a249d1e02baa97a6e9e8aada548adf809fae43eb>
2020-07-30 17:50:24 +0300

    integrating bootstrap

--- src/component/link/LinkComponent.js
+++ src/component/link/LinkComponent.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const LinkComponent = ({ title, href, type}) => {
     return (
         <div className={styles[`${type}-list-item`]}>
-            <Link to={href} className={styles[`${type}-lin`]}>
+            <Link to={href} className={styles[`${type}-link`]}>
                 {title}
             </Link>
         </div>
